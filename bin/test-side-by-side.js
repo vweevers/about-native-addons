490b1700a7fd99fd4a5bb624c6d181adc676cbe1
Vincent Weevers <mail@vincentweevers.nl>
2019-11-03 14:36:01 +0100

    Add ability to run side-by-side test on npm packages

--- bin/test-side-by-side.js
+++ bin/test-side-by-side.js
@@ -8,7 +8,13 @@ const runTests = promisify(require('../lib/run-tests'))
 const createProject = promisify(require('../lib/project').from)
 
 ;(async function () {
-  const projects = await Promise.all(process.argv.slice(2).map(spec => {
+  // Should introduce minimist here
+  const argv = process.argv.slice(2)
+  const fromNpm = argv[0] === '--npm'
+
+  if (fromNpm) argv.shift()
+
+  const projects = await Promise.all(argv.map(spec => {
     return createProject(spec)
   }))
 
@@ -19,7 +25,7 @@ const createProject = promisify(require('../lib/project').from)
 
   const testMatrix = await testPairs(projects, async function (a, b) {
     console.error('Test %s with injected %s', a.title, b.title)
-    return runTests(a.pkg, { inject: b.pkg })
+    return runTests(a.pkg, { inject: b.pkg, fromNpm })
   })
 
   console.log(table(testMatrix))
